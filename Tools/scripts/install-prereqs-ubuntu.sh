937c9869897f172626f9cb631c7dc003aa60d8eb
hiro2233 <htakey@gmail.com>
2016-01-31 15:18:02 -0400

    updated travis.yml

--- Tools/scripts/install-prereqs-ubuntu.sh
+++ Tools/scripts/install-prereqs-ubuntu.sh
@@ -57,7 +57,7 @@ fi
 
 sudo usermod -a -G dialout $USER
 
-$APT_GET remove modemmanager
+#$APT_GET remove modemmanager
 $APT_GET update
 $APT_GET install $BASE_PKGS $SITL_PKGS $PX4_PKGS $BEBOP_PKGS $UBUNTU64_PKGS $AVR_PKGS
 sudo pip2 -q install $PYTHON_PKGS
