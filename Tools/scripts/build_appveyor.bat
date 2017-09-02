bde02e0b1f4e85d19c179b4b0dc61918bbed1d69
Hiroshi Takey (hiro2233) <htakey@gmail.com>
2017-09-02 04:12:39 -0400

    Tools: updated AppVeyor build script.

--- Tools/scripts/build_appveyor.bat
+++ Tools/scripts/build_appveyor.bat
@@ -2,8 +2,10 @@ goto %TOOLSET%
 
 :cygwin
 
-C:\cygwin64\setup-x86_64.exe -qnNdO -R C:\cygwin64 -s http://cygwin.mirror.constant.com -l C:\cygwin64\var\cache\setup -P gcc-g++,gcc-core,binutils,libxml2,libxml2-devel,libxslt-devel,python2,python2-devel,python2-pip,git,ccache
-path C:\cygwin64\bin;%path%
+move C:\cygwin C:\cygwin_bk
+move C:\cygwin64 C:\cygwin
+C:\cygwin\setup-x86_64.exe -qnNdO -R C:\cygwin -s http://cygwin.mirror.constant.com -l C:\cygwin\var\cache\setup -P gcc-g++,gcc-core,binutils,libxml2,libxml2-devel,libxslt-devel,python2,python2-devel,python2-pip,git,ccache
+path C:\cygwin\bin;%path%
 set CHERE_INVOKING=yes
 bash -lc "pip2 install empy pyserial future lxml"
 bash -lc "pip2 install mavproxy"
