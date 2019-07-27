dca0652157c9083b9600ce01cdfffb26a0748551
Hiroshi Takey (hiro2233) <htakey@gmail.com>
2019-07-27 03:16:25 -0400

    Tools: joypad_remote: added Parameters.

---
+++ Tools/joypad_remote/Parameters.cpp
@@ -0,0 +1,80 @@
+#include "joypad_remote.h"
+
+/*
+ *  AntennaTracker parameter definitions
+ *
+ */
+
+#define GSCALAR(v, name, def) { joypadremote.g.v.vtype, name, Parameters::k_param_ ## v, &joypadremote.g.v, {def_value : def} }
+#define ASCALAR(v, name, def) { joypadremote.aparm.v.vtype, name, Parameters::k_param_ ## v, (const void *)&joypadremote.aparm.v, {def_value : def} }
+#define GGROUP(v, name, class) { AP_PARAM_GROUP, name, Parameters::k_param_ ## v, &joypadremote.g.v, {group_info : class::var_info} }
+#define GOBJECT(v, name, class) { AP_PARAM_GROUP, name, Parameters::k_param_ ## v, (const void *)&joypadremote.v, {group_info : class::var_info} }
+#define GOBJECTN(v, pname, name, class) { AP_PARAM_GROUP, name, Parameters::k_param_ ## pname, (const void *)&joypadremote.v, {group_info : class::var_info} }
+
+const AP_Param::Info JoypadRemote::var_info[] PROGMEM = {
+    // @Param: FORMAT_VERSION
+    // @DisplayName: Eeprom format version number
+    // @Description: This value is incremented when changes are made to the eeprom format
+    // @User: Advanced
+    GSCALAR(format_version, "FORMAT_VERSION", 0),
+
+    // @Param: SYSID_SW_TYPE
+    // @DisplayName: Software Type
+    // @Description: This is used by the ground station to recognise the software type (eg ArduPlane vs ArduCopter)
+    // @Values: 0:ArduPlane,4:AntennaTracker,10:Copter,20:Rover
+    // @User: Advanced
+    GSCALAR(software_type,  "SYSID_SW_TYPE",  Parameters::k_software_type),
+
+    // RC channel
+    //-----------
+    // @Group: RC1_
+    // @Path: ../libraries/RC_Channel/RC_Channel.cpp
+    GGROUP(rc_1,    "RC1_", RC_Channel),
+    // @Group: RC2_
+    // @Path: ../libraries/RC_Channel/RC_Channel.cpp
+    GGROUP(rc_2,    "RC2_", RC_Channel),
+    // @Group: RC3_
+    // @Path: ../libraries/RC_Channel/RC_Channel.cpp
+    GGROUP(rc_3,    "RC3_", RC_Channel),
+    // @Group: RC4_
+    // @Path: ../libraries/RC_Channel/RC_Channel.cpp
+    GGROUP(rc_4,    "RC4_", RC_Channel),
+    // @Group: RC5_
+    // @Path: ../libraries/RC_Channel/RC_Channel.cpp,../libraries/RC_Channel/RC_Channel_aux.cpp
+    GGROUP(rc_5,    "RC5_", RC_Channel),
+    // @Group: RC6_
+    // @Path: ../libraries/RC_Channel/RC_Channel.cpp,../libraries/RC_Channel/RC_Channel_aux.cpp
+    GGROUP(rc_6,    "RC6_", RC_Channel),
+    // @Group: RC6_
+    // @Path: ../libraries/RC_Channel/RC_Channel.cpp,../libraries/RC_Channel/RC_Channel_aux.cpp
+    GGROUP(rc_7,    "RC7_", RC_Channel),
+
+    // @Param: CMD_TOTAL
+    // @DisplayName: Number of loaded mission items
+    // @Description: Set to 1 if HOME location has been loaded by the ground station. Do not change this manually.
+    // @Range: 1 255
+    // @User: Advanced
+    GSCALAR(command_total,  "CMD_TOTAL",    0),
+
+    AP_VAREND
+};
+
+void JoypadRemote::load_parameters(void)
+{
+    if (!g.format_version.load() ||
+        g.format_version != Parameters::k_format_version) {
+
+        // erase all parameters
+        //hal.console->printf_PS(PSTR("Firmware change: erasing EEPROM...\n"));
+        AP_Param::erase_all();
+
+        // save the current format version
+        g.format_version.set_and_save(Parameters::k_format_version);
+        hal.console->printf_PS(PSTR("ok.\n"));
+    }
+
+    //uint32_t before = AP_HAL::micros();
+    // Load all auto-loaded EEPROM variables
+    AP_Param::load_all();
+    //hal.console->printf_PS(PSTR("load_all took %luus\n"), (unsigned long)(AP_HAL::micros() - before));
+}
